aac9152236735825692d5b9c261c8b3c623006aa
Ezequiel Mazzini <ezequiel.mazzini@bue.edu.ar>
2022-10-15 13:49:13 -0300

    mejoras en selecciones y contactos

---
+++ js/datos-jugadores.js
@@ -0,0 +1,98 @@
+var data= [
+    {
+    nombre: "Facundo Campazzo",
+    camiseta: "7",
+    equipo: "Agente libre",
+    posicion: "Base",
+    altura: "1.78",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Nicolás Laprovittola",
+    camiseta: "8",
+    equipo: "FC Barcelona",
+    posicion: "Base",
+    altura: "1.93",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "José Vildoza",
+    camiseta: "11",
+    equipo: "Flamengo",
+    posicion: "Base",
+    altura: "1.91",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Máximo Fjellerup",
+    camiseta: "2",
+    equipo: "Bàsquet Girona",
+    posicion: "Escolta",
+    altura: "1.98",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Carlos Delfino",
+    camiseta: "10",
+    equipo: "Victoria Libertas Pesaro",
+    posicion: "Escolta",
+    altura: "2.01",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Nicolás Brussino",
+    camiseta: "9",
+    equipo: "CB Gran Canaria",
+    posicion: "Escolta",
+    altura: "2.04",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Gabriel Deck",
+    camiseta: "14",
+    equipo: "Real Madrid",
+    posicion: "Ala Pivote",
+    altura: "1.98",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Juan Pablo Vaulet",
+    camiseta: "22",
+    equipo: "Bàsquet Manresa",
+    posicion: "Alero",
+    altura: "1.99",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Leandro Bolmaro",
+    camiseta: "31",
+    equipo: "Utha Jazz",
+    posicion: "Alero",
+    altura: "1.98",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Tomás Chapero",
+    camiseta: "25",
+    equipo: "Club Ourense Baloncesto",
+    posicion: "Ala Pivot",
+    altura: "2.06",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Marcos Delía",
+    camiseta: "12",
+    equipo: "BC Wolves",
+    posicion: "Pivot",
+    altura: "2.11",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+    {
+    nombre: "Tayavek Gallizzi",
+    camiseta: "83",
+    equipo: "Instituto de Córdoba",
+    posicion: "Pivot",
+    altura: "2.05",
+    imagen: "imagenes/basketball-player-200x200.jpg"
+    },
+]
